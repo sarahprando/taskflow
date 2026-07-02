c018c7fa5457e3fa71f278d14cdc0eb7e32e0c5c
Sarah Prando <sarahprando40@gmail.com>
2026-07-02 17:23:54 -0300

    feat: add task controller, database config and swagger setup

---
+++ src/main/java/com/sarahprando/taskflow/controller/TaskController.java
@@ -0,0 +1,46 @@
+package com.sarahprando.taskflow.controller;
+
+import com.sarahprando.taskflow.entity.Task;
+import com.sarahprando.taskflow.service.TaskService;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    List<Task> create(@RequestBody Task task) {
+        return taskService.create(task);
+    }
+
+    @GetMapping
+    List<Task> list() {
+        return taskService.list();
+    }
+
+    @PutMapping
+    List<Task> update(@RequestBody Task task) {
+        return taskService.update(task);
+
+    }
+
+    @DeleteMapping("/{id}")
+    List<Task> delete(@PathVariable("id") Long id) {
+        return taskService.delete(id);
+    }
+}
